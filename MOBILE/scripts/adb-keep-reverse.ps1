a4eeea68a63113ea879a7d7ce637c9727640ca79
GilvanJSSousa <gillvanjs@gmail.com>
2025-11-18 23:34:13 -0300

    up

--- MOBILE/scripts/adb-keep-reverse.ps1
+++ MOBILE/scripts/adb-keep-reverse.ps1
@@ -7,22 +7,34 @@ param(
 function EnsureReverse {
   $list = adb reverse --list 2>$null
   if ($null -eq $list -or ($list -notmatch "tcp:$Port")) {
+    Write-Host "[adb-keeper] Aplicando reverse tcp:$Port -> tcp:$Port"
     adb reverse tcp:$Port tcp:$Port | Out-Null
+  } else {
+    Write-Host "[adb-keeper] Reverse tcp:$Port ativo"
   }
 }
 
-if ($Device -ne "") {
-  adb tcpip 5555 | Out-Null
-  Start-Sleep -Seconds 1
-  adb connect $Device | Out-Null
+function Connected {
+  $devices = adb devices 2>$null
+  return ($devices -match "\bdevice\b")
 }
 
-EnsureReverse
+function ConnectDevice($dev) {
+  if ([string]::IsNullOrWhiteSpace($dev)) { return }
+  Write-Host "[adb-keeper] Conectando ao device $dev"
+  adb tcpip 5555 | Out-Null
+  Start-Sleep -Seconds 1
+  adb connect $dev | Out-Null
+}
+
+if ($Device -ne "") { ConnectDevice $Device }
+
+if (Connected) { EnsureReverse } else { Write-Host "[adb-keeper] Nenhum device conectado; aguardando..." }
 
 while ($true) {
   Start-Sleep -Seconds $IntervalSeconds
-  $devices = adb devices 2>$null
-  if ($devices -match "\bdevice\b") {
-    EnsureReverse
+  if (-not (Connected)) {
+    if ($Device -ne "") { ConnectDevice $Device }
   }
+  if (Connected) { EnsureReverse }
 }
