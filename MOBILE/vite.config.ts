2449aee8b122bd01ec548f0e0aa9ecc7609a0846
gillvanjs <gillvanjs@gmail.com>
2025-11-19 01:11:12 +0000

    Novo modo de server IIII

--- MOBILE/vite.config.ts
+++ MOBILE/vite.config.ts
@@ -1,5 +1,4 @@
 // Configuração Vite/Esbuild: adicionar target ES2020
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -28,11 +27,6 @@ export default defineConfig(({ mode }) => {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
       },
       test: {
         environment: 'jsdom'
